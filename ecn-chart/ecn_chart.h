d7330c2865eb17a1e617c395c59cee539317559c
Stanislav Buldakov <s.buldakov@galament.com>
2017-11-21 15:07:33 +0400

    Initial Commit

---
+++ ecn-chart/ecn_chart.h
@@ -0,0 +1,19 @@
+//
+//  ecn_chart.h
+//  ecn-chart
+//
+//  Created by Stas Buldakov on 21.11.17.
+//  Copyright © 2017 Galament. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ecn_chart.
+FOUNDATION_EXPORT double ecn_chartVersionNumber;
+
+//! Project version string for ecn_chart.
+FOUNDATION_EXPORT const unsigned char ecn_chartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ecn_chart/PublicHeader.h>
+
+
